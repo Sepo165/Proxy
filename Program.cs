3c0b3dde59a11047ce53d069ee73bcdbf4a7aa00
Sepo165 <72491571+Sepo165@users.noreply.github.com>
2020-10-07 10:21:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using Microsoft.Win32;
+
+namespace Proxy
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Ping myPing = new Ping();
+            PingReply pingReply = myPing.Send("IP");
+            if (pingReply.Status == IPStatus.Success)
+                Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", "ProxyEnable", 0, RegistryValueKind.DWord);
+            else
+                Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", "ProxyEnable", 1, RegistryValueKind.DWord);
+        }
+    }
+}
